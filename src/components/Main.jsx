535d4c132213fa231d25850f3b02a39335c1b690
Fuad Maulana <fuadm03@gmail.com>
2023-09-13 17:57:34 +0700

    feat: empty notes list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,14 @@
 import NoteInput from './NoteInput';
+import NotesList from './NotesList';
 
 const Main = () => {
     return (
         <div className="note-app__body">
             <NoteInput />
+            <h2>Catatan Aktif</h2>
+            <NotesList />
+            <h2>Arsip</h2>
+            <NotesList />
         </div>
     );
 }
---
+++ src/components/NotesList.jsx
@@ -0,0 +1,7 @@
+const NotesList = () => {
+    return (
+        <p className="notes-list__empty-message">Tidak ada catatan</p>
+    );
+}
+
+export default NotesList;
