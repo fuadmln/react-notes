f82af6b32a3d1d3ada500ad1d133346ff1c45c13
Fuad Maulana <fuadm03@gmail.com>
2023-09-15 16:40:20 +0700

    refactor: avoid magic number, give context to 50 is max input character lenght

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 const NoteInput = ({setNotes}) => {
+    const maxTitleLength = 50;
     const [formData, setFormData] = useState({title: '', body: ''});
 
     const changeTitleHandler = (e) => {
-        if(e.target.value.length > 50) return;
+        if(e.target.value.length > maxTitleLength) return;
 
         setFormData(data => ({...data, title: e.target.value}));
     }
@@ -31,7 +32,7 @@ const NoteInput = ({setNotes}) => {
             <h2>Buat Catatan</h2>
             <form onSubmit={submitHandler}>
                 <p className="note-input__title__char-limit">
-                    Sisa karakter: { formData.title.length ? 50-formData.title.length : 50 }
+                    Sisa karakter: { formData.title.length ? maxTitleLength-formData.title.length : maxTitleLength }
                 </p>
                 <input 
                     type="text" 
